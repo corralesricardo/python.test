90b9e0ef53fffeccb42fe15ef3051c2571fe8c30
ricardo corrales <92402662+corralesricardo@users.noreply.github.com>
2022-03-15 07:13:20 -0300

    Add files via upload

---
+++ test001.py
@@ -0,0 +1,25 @@
+###Funcion
+def saludo(mensaje):
+    print ('#hola')
+    print(mensaje)
+#######################
+pesos = input("¿Cuantos pesos Argentinos tienes? ")
+pesos = float(pesos)
+valor_dolar = 190
+dolares = pesos / valor_dolar
+dolares = str(dolares)
+print("Tienes $" + dolares+" dolares.")
+
+dolares = float(dolares)
+if dolares > 1000:
+    saludo("#mayorMil")
+    print("Tenes mas de Mil dolares!!!")
+elif dolares == 1000:
+    saludo("#justoMIL")
+    print("Tenes Justo Mil dolares!!!")
+else:
+    saludo("#MenorMil")
+    print("Tenes menos de Mil dolares!!!")
+
+print("SALI?")
+print('Saliendo')
